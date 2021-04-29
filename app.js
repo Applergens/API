910f580ea9fda774599c70f08c8bbdfd7fc9b26a
AmadorMartinez10 <ttore10.amc@gmail.com>
2021-04-29 17:10:18 +0200

    CORS added

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Imports
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Server instance
-const app = express()
+const app = express();
 
 // Global variables
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+// ENDPOINTS START
 app.get('/', (req, res) => {
   res.send('API working successful!')
 })
