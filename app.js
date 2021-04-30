383277c81d58e9a81aa0f0995da3c5270054d640
imkrysis <elkrysss@gmail.com>
2021-04-30 15:53:32 +0200

    Heroku test

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
+app.use((req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  
+  next();
+  
+})
+
 // ENDPOINTS START
 app.get('/', (req, res) => {
 
